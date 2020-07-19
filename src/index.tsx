c575407a1691f4307bc741195f367a5ccec45f59
LeeHyengTae <tedd911@icloud.com>
2020-07-19 18:12:45 +0900

    18:12

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ScrollContext } from 'react-router-scroll-4'
+
+class Root extends Component {
+    render() {
+        return (
+        <BrowserRouter>
+            <ScrollContext>
+                <Switch>
+                <Route path={`/`} component={Login} />
+                    <App />
+                </Switch>
+            </ScrollContext>
+        </BrowserRouter>
+        )
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

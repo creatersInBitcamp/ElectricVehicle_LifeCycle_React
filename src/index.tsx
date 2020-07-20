6f6c5d1657874dad31af6749bfb2ad7fc034535c
이준화 <62980975+wnsghk16@users.noreply.github.com>
2020-07-20 17:39:19 +0900

    200720 17:39

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css';
-import App from './App';
 import { ScrollContext } from 'react-router-scroll-4'
+import Map from "./map/map";
 
 class Root extends Component {
     render() {
@@ -11,8 +11,7 @@ class Root extends Component {
         <BrowserRouter>
             <ScrollContext>
                 <Switch>
-                {/*<Route path={`/`} component={Login} />*/}
-                    <App />
+                <Route path={`/`} component={Map} />
                 </Switch>
             </ScrollContext>
         </BrowserRouter>

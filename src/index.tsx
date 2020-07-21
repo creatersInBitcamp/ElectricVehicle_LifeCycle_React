2d7e2288b478cdcffe8a7873eb7a18f7e84b9e44
jusun <nmming@naver.com>
2020-07-21 10:10:57 +0900

    수정
    수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,28 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import './index.scss';
-=======
-<<<<<<< Updated upstream
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import './index.css';
-=======
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ScrollContext } from 'react-router-scroll-4';
 import './index.scss';
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-=======
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import './index.css';
->>>>>>> parent of f8d2434... 20200720
-=======
-import './index.scss';
->>>>>>> Stashed changes
-import App from './App';
+import Login from "./login";
+
 
 class Root extends Component {
     render() {
         return (
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-        <div>
-            <App/>
-        </div>
-=======
-<<<<<<< Updated upstream
-        <BrowserRouter>
-            <ScrollContext>
-                <Switch>
-                <Route path={`/`} component={Login} />
-=======
-        <BrowserRouter>
-            <ScrollContext>
-                <Switch>
-                <Route path={`/`} />
->>>>>>> parent of f8d2434... 20200720
-                    <App />
-                </Switch>
-            </ScrollContext>
-        </BrowserRouter>
-<<<<<<< HEAD
-=======
-        <div>
-           <App/>
-        </div>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-=======
->>>>>>> parent of f8d2434... 20200720
-=======
-        <div>
-            <App/>
-        </div>
->>>>>>> Stashed changes
+            <BrowserRouter basename={'/'}>
+                <ScrollContext>
+                    <Switch>
+                        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Login} />
+                    </Switch>
+                </ScrollContext>
+            </BrowserRouter>
         )
     }
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Root/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Root/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );

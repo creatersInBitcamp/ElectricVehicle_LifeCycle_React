f5771dbfcf52cb7bea4ddced93bdd5128938c639
LeeHyengTae <tedd911@icloud.com>
2020-08-10 18:08:19 +0900

    이미지 삭제 실패.
    18:08

--- src/newPurchase/left-sidebar.js
+++ src/newPurchase/left-sidebar.js
@@ -5,22 +5,51 @@ import {useDispatch, useSelector} from "react-redux";
 import { useRouteMatch } from 'react-router-dom';
 
 // import custom Components
-import NewProduct from "../common/new-product";
 import Breadcrumb from "../common/breadcrumb";
 import DetailsWithPrice from "./common/product/details-price";
 import ImageZoom from './common/product/image-zoom'
 import SmallImages from './common/product/small-image'
-import MyCar from "./MyCar";
-import Service from "./common/service";
 import DetailsTopTabs from "./common/details-top-tabs";
 import {addToCart} from "../cart/cartReducer";
 import {addToCartUnsafe} from "../cart/cartReducer";
 import {addToWishlist} from "../wishlist/wishlistReducer";
-import {posts} from "../board/items/data";
 import Recent from "../board/items/recent";
+
+const initinalState = [
+        {
+            postId: 1,
+            userId: "tedd911",
+            title: "쏘울(SOUL) EV 시승기 – 감성과 테크놀로지의 조화",
+            name: "이형태",
+            link: "https://www.evpost.co.kr/wp/쏘울soul-ev-시승기-감성과-테크놀로지의-조화/",
+            img: "https://d3jn14jkdoqvmm.cloudfront.net/wp/wp-content/uploads/2020/05/13112107/05-13-%ED%95%9C%EC%9A%A9%EB%8D%95-%EC%8F%98%EC%9A%B8-EV-%EC%8B%9C%EC%8A%B9%EA%B8%B0-%EA%B0%90%EC%84%B1%EA%B3%BC-%ED%85%8C%ED%81%AC%EB%86%80%EB%A1%9C%EC%A7%80%EC%9D%98-%EC%A1%B0%ED%99%94-696x365.jpg",
+            hits: 123,
+            like: 12,
+            content: 'post 글쓴이의 글',
+            comments: [
+                {
+                    commentId: 1,
+                    userId: 'wnsghk16',
+                    comment: '코멘트 출력 테스트'
+                },
+                {
+                    commentId: 2,
+                    userId: 'tedd911',
+                    comment: '코멘트 출력 테스트2'
+                },
+                {
+                    commentId: 3,
+                    userId: 'karkky',
+                    comment: '다시 연습 중.'
+                }
+            ],
+            dateTime: "time",
+        },
+    ]
 
 const LeftSideBar = () => {
     const [state, setState] = useState({ nav1: null, nav2: null });
+    const [posts, setposts] = useState(initinalState)
     const slider1 = useRef();
     const slider2 = useRef();
 

6fda5b187aa47d46df2ecfbfbf819f5a5c61c27e
LeeHyengTae <tedd911@icloud.com>
2020-08-18 20:49:37 +0900

    my account

--- src/user/MyAccount.jsx
+++ src/user/MyAccount.jsx
@@ -6,6 +6,7 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import MyAccount2 from "./MyAccount2";
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -36,14 +37,6 @@ function a11yProps(index) {
 
 export const MyAccount = () => {
     const [value, setValue] = useState(0)
-    const [name, setName] = useState('')
-    const [userId, setUserId] = useState('')
-    const [email, setEmail] = useState('')
-    const [sex, setSex] = useState('')
-    const [number, setNumber] = useState('')
-    const [birth, setBirth] = useState('')
-    const [addr, setAddr] = useState('')
-    const [image, setImage] = useState('')
     const [posts, setPosts] = useState([])
     const [comments, setComments] = useState([])
     const [bookmarks, setBookmarks] = useState([])
@@ -55,14 +48,6 @@ export const MyAccount = () => {
         setUserSession(JSON.parse(sessionStorage.getItem("user")))
     },[])
     useEffect(()=> {
-        setName(userSession.name)
-        setUserId(userSession.userId)
-        setEmail(userSession.email)
-        setSex(userSession.sex)
-        setNumber(userSession.phoneNumber)
-        setBirth(userSession.birthDate)
-        setAddr(userSession.addr)
-        setImage(userSession.profileImage)
         setPosts(userSession.postList)
         setComments(userSession.commentList)
         setBookmarks(userSession.bookmarkList)
@@ -96,139 +81,49 @@ export const MyAccount = () => {
                                     </span>
                                     </div>
                                     <div className="block-content">
-                                        <ul>
-                                            <li className="active"><Link to={"/pages/profile"}>Account Info</Link></li>
-                                            <li><Link to={"/pages/myCar"}><i className="fa fa-car"/> My Car {(useds !== undefined)? useds.length : ""}</Link></li>
-                                            <li><a href="#"><i className="fa fa-file-code-o"/> My Orders {(purchase !== undefined) ? purchase.length : ""}</a></li>
-                                            <li><a href="#"><i className="fa fa-list-alt"/> My Wishlist</a></li>
-                                            <li><a href="#"><i className="fa fa-clipboard"/> My Post {(posts !== undefined) ? posts.length : ""}</a></li>
-                                            <li><a href="#"><i className="fa fa-comment-o"/> My Comment {(comments !== undefined) ? comments.length : ""}</a></li>
-                                            <li><a href="#"><i className="fa fa-bookmark-o"/> My Bookmark {(bookmarks !== undefined) ? bookmarks.length : ""}</a></li>
-                                            <li><Link to={"/pages/profile"}><i className="fa fa-address-book"/> My Account</Link></li>
-                                            <li><a href="#">Change Password</a></li>
-                                            <li className="last"><a href="#">Log Out</a></li>
-                                        </ul>
+                                        <Tabs
+                                            orientation="vertical"
+                                            variant="scrollable"
+                                            value={value}
+                                            onChange={handleChange}
+                                            aria-label="Vertical tabs example"
+                                        >
+                                            <Tab label="My Account" {...a11yProps(0)} />
+                                            <Tab label="My Car" {...a11yProps(1)} />
+                                            <Tab label="My Wishlist" {...a11yProps(2)} />
+                                            <Tab label="My Post" {...a11yProps(3)} />
+                                            <Tab label="My Comment" {...a11yProps(4)} />
+                                            <Tab label="My Bookmark" {...a11yProps(5)} />
+                                            <Tab label="Change Password" {...a11yProps(6)} />
+                                        </Tabs>
                                     </div>
                                 </div>
-                                <Tabs
-                                    orientation="vertical"
-                                    variant="scrollable"
-                                    value={value}
-                                    onChange={handleChange}
-                                    aria-label="Vertical tabs example"
-                                >
-                                    <Tab label="My Account" {...a11yProps(0)} />
-                                    <Tab label="My Car" {...a11yProps(1)} />
-                                    <Tab label="My Wishlist" {...a11yProps(2)} />
-                                    <Tab label="My Post" {...a11yProps(3)} />
-                                    <Tab label="My Comment" {...a11yProps(4)} />
-                                    <Tab label="My Bookmark" {...a11yProps(5)} />
-                                    <Tab label="Change Password" {...a11yProps(6)} />
-                                </Tabs>
                             </div>
                             <div className="col-lg-9">
                                 <div className="dashboard-right">
                                     <div className="dashboard">
                                         <div className="container-fluid">
                                                 <TabPanel value={value} index={0}>
-                                                 <div className="row">
-                                                    <div className="col-xl-4">
-                                                        <div className="card">
-                                                            <div className="card-body">
-                                                                <div className="profile-details text-center">
-                                                                    <img src={image} alt="" className="img-fluid img-90 rounded-circle blur-up lazyloaded" />
-                                                                    <h5 className="f-w-600 f-16 mb-0">{name}</h5>
-                                                                    <span>ID : {userId}</span>
-                                                                </div>
-                                                                <hr />
-                                                                <div className="project-status">
-                                                                    <h5 className="f-w-600 f-16">자기소개</h5>
-                                                                    <div className="media">
-                                                                        <div className="media-body">
-                                                                            <h6>Performance <span className="pull-right">80%</span></h6>
-                                                                            <div className="progress sm-progress-bar">
-                                                                                <div className="progress-bar bg-primary" role="progressbar" style={{width: '90%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"/>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div className="media">
-                                                                        <div className="media-body">
-                                                                            <h6>Overtime <span className="pull-right">60%</span></h6>
-                                                                            <div className="progress sm-progress-bar">
-                                                                                <div className="progress-bar bg-secondary" role="progressbar" style={{width: '60%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"/>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div className="media">
-                                                                        <div className="media-body">
-                                                                            <h6>Leaves taken <span className="pull-right">50%</span></h6>
-                                                                            <div className="progress sm-progress-bar">
-                                                                                <div className="progress-bar bg-danger" role="progressbar" style={{width: '50%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"/>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-xl-8">
-                                                        <div className="card profile-card">
-                                                            <div className="card-body">
-                                                                <div className="tab-pane fade show active">
-                                                                    <h5 className="f-w-600 f-16">마이페이지</h5>
-                                                                    <div className="table-responsive profile-table">
-                                                                        <table className="table table-responsive">
-                                                                            <tbody>
-                                                                            <tr>
-                                                                                <td>이름</td>
-                                                                                <td>{name}</td>
-                                                                            </tr>
-                                                                            <tr>
-                                                                                <td>이메일</td>
-                                                                                <td>{email}</td>
-                                                                            </tr>
-                                                                            <tr>
-                                                                                <td>성별</td>
-                                                                                <td>{sex}</td>
-                                                                            </tr>
-                                                                            <tr>
-                                                                                <td>전화번호</td>
-                                                                                <td>{number}</td>
-                                                                            </tr>
-                                                                            <tr>
-                                                                                <td>생년월일</td>
-                                                                                <td>{birth}</td>
-                                                                            </tr>
-                                                                            <tr>
-                                                                                <td>주소</td>
-                                                                                <td>{addr}</td>
-                                                                            </tr>
-                                                                            </tbody>
-                                                                        </table>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                 </div>
+                                                    My Account
+                                                    <MyAccount2 user={userSession} />
                                                 </TabPanel>
                                                 <TabPanel value={value} index={1}>
-                                                    Item Two
+                                                    My CAR
                                                 </TabPanel>
                                                 <TabPanel value={value} index={2}>
-                                                    Item Three
+                                                    My WishList
                                                 </TabPanel>
                                                 <TabPanel value={value} index={3}>
-                                                    Item Four
+                                                    My Post
                                                 </TabPanel>
                                                 <TabPanel value={value} index={4}>
-                                                    Item Five
+                                                    My Comment
                                                 </TabPanel>
                                                 <TabPanel value={value} index={5}>
-                                                    Item Six
+                                                    My bookmark
                                                 </TabPanel>
                                                 <TabPanel value={value} index={6}>
-                                                    Item Seven
+                                                    Change password
                                                 </TabPanel>
                                         </div>
                                     </div>

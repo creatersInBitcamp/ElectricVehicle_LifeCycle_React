a56c92b7dbe62604af83b84904eb4370223cadae
이준화 <62980975+wnsghk16@users.noreply.github.com>
2020-08-19 09:54:38 +0900

    Update shop.js

--- src/atomic/api/shop.js
+++ src/atomic/api/shop.js
@@ -1,16 +1,5 @@
-/**
- * Mocking client-server processing
- */
-// import _products from './data.json'
 /*
 import _products from './test.json'
-
-const TIMEOUT = 100
-
-export default {
-    getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
-    buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
-}
 */
 import axios from "axios";
 

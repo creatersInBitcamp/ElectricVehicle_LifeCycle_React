dae249a180f1052a17e33d2caeb4862fd0b41e39
곽경열 <62980973+kwakky1@users.noreply.github.com>
2020-08-19 19:55:01 +0900

    user 차트완성, 데이터를 만들어야한다.

--- src/admin/page/User.jsx
+++ src/admin/page/User.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {AdminBreadcrumb} from '../common';
 import {Table} from '../item'
+import {Bar, Doughnut} from "react-chartjs-2";
 import axios from "axios";
-import {Line} from "react-chartjs-2";
-import {buyOption} from "../../atomic/constants/chartData";
+import {lineOptions} from "../../atomic/constants/chartData";
 const userReducer = ( state= {}, action ) => {
     switch (action.type) {
         default: return state
@@ -14,6 +14,14 @@ export const User = () => {
 
     const[data,setData] = useState([])
 
+    const makeColors = () => {
+        let r = Math.floor(Math.random() * 255)
+        let g = Math.floor(Math.random() * 255)
+        let b = Math.floor(Math.random() * 255)
+        return "rgb(" + r + "," + g + "," + b + ")"
+    }
+
+    // 유저 테이블
     useEffect(() => {
         axios.get(`http://localhost:8080/user/findAll`)
             .then((res)=>{
@@ -61,35 +69,130 @@ export const User = () => {
                     setData([...dataUpdate])
                     resolve()
                     axios.post(`http://localhost:8080/user/allUpdate`, [...dataUpdate])
-                            .then((res) => {
-                            })
-                            .catch(() => {
-                                alert("통신실패")
-                            })
+                        .then((res) => {
+                        })
+                        .catch(() => {
+                            alert("통신실패")
+                        })
 
                 }, 1000)
             })
     }
+
+    const[ageChart,setAgeChart] = useState({})
+    const[ageChartData, setAgeCharData] = useState({})
+
+    //연령대별 차트
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8080/user/countAge`)
+            .then((res)=>{
+                setAgeChart(res.data)
+            }).catch((err)=>{
+                throw err
+        })
+    },[])
+
+    useEffect(()=>{
+
+        const ageKeys = []
+        const ageValues = []
+        const ageColor = []
+
+        for(let i in ageChart) {
+            ageKeys.push(ageChart[i].AGE+"대")
+            ageValues.push(ageChart[i].CNT)
+            ageColor.push(makeColors())
+        }
+        setAgeCharData({
+            labels: ageKeys,
+            datasets:[
+                {
+                    data: ageValues,
+                    backgroundColor: ageColor
+                },
+            ],
+        })
+    },[ageChart])
+
+    const[sexChart, setSexChart] = useState({})
+    const[sexChartData, setSexChartData] = useState({})
+
+    // 남녀 성비차트
+    useEffect(()=>{
+        axios.get(`http://localhost:8080/user/countSex`)
+            .then((res)=>{
+                console.log(res.data)
+                setSexChart(res.data)
+            })
+    },[])
+
+    useEffect(()=>{
+
+        const sexKeys = []
+        const sexValues = []
+
+        for(let i in sexChart) {
+            sexKeys.push(sexChart[i].SEX)
+            sexValues.push(sexChart[i].COUNT)
+        }
+        setSexChartData({
+            labels: sexKeys,
+            datasets:[
+                {
+                    data: sexValues,
+                    backgroundColor: [
+                        '#36A2EB',
+                        '#FF6384',
+                    ],
+                    hoverBackgroundColor: [
+                        '#36A2EB',
+                        '#FF6384',
+                    ]
+                },
+            ],
+        })
+    },[sexChart])
+
         return (
             <>
                 <AdminBreadcrumb title="사용자 현황" parent="Users" />
                 <div className="container-fluid">
-                    <div className="card">
-                        <div className="card-header">
-                            <h5>사용자 정보</h5>
-                        </div>
+                    <div className="row">
                         <div className="col-sm-12">
-                            <div className="card">
-                                <div className="card-body sell-graph">
+
                                 </div>
-                            </div>
-                        </div>
-                        <div className="card-body">
-                            <div className="clearfix"/>
-                            <div id="batchDelete" className="category-table user-list order-table coupon-list-delete">
-                                <Table title={"사용자"} data={data} columns={columns} editable={editable} />
-                            </div>
-                        </div>
+                                <div className="col-xl-6 xl-100">
+                                    <div className="card">
+                                        <div className="card-header">
+                                            <h5>사용자 연령분포</h5>
+                                        </div>
+                                        <div className="card-body">
+                                            <div className="market-chart">
+                                                <Bar data={ageChartData} options={{
+                                                    dragData: true, dragDataRound: 0, legend:{ display: false}}} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-xl-6 xl-100">
+                                    <div className="card">
+                                        <div className="card-header">
+                                            <h5>사용자 성비</h5>
+                                        </div>
+                                        <div className="card-body">
+                                            <div className="user-status table-responsive latest-order-table">
+                                                <Doughnut data={sexChartData} options={{dragData: true, dragDataRound: 0}}/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card-body">
+                                    <div className="clearfix"/>
+                                    <div id="batchDelete" className="category-table user-list order-table coupon-list-delete">
+                                        <Table title={"사용자"} data={data} columns={columns} editable={editable} />
+                                    </div>
+                                </div>
                     </div>
                 </div>
             </>

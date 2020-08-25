6339338df119d00e5f49f4647327b0053593f797
곽경열 <62980973+kwakky1@users.noreply.github.com>
2020-08-25 21:50:04 +0900

    이메일보내기 버튼 하나로 통합 // 버튼 누른 후 hidden

--- src/user/Forget-password.jsx
+++ src/user/Forget-password.jsx
@@ -5,45 +5,39 @@ import axios from 'axios'
 
 export const ForgetPassword = () => {
     const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [hideButton, setHideButton] = useState(false)
 
     const history = useHistory()
 
-    const findPassword = (e) => {
-        e.preventDefault()
-        axios.get(`http://localhost:8080/user/sendPassword/${email}`)
-            .then((res)=>{
-                if(res.data !== ''){
-                    alert("이메일정보가 확인되었습니다. 이메일보내기를 눌러주세요!")
-                    setPassword(res.data)
-                } else {
-                    alert("이메일 정보가 없습니다.")
-                }
-            })
-            .catch(()=>{
-                alert("통신실패")
-                window.location.reload()
-            })
-    }
-
     const sendEmail = (e) => {
         e.preventDefault()
-        const info = {
-            email : email,
-            password: password
-        }
+        if(email !== ''){
+            setHideButton(true)
+            axios.get(`http://localhost:8080/user/sendPassword/${email}`)
+                .then((res)=>{
+                    const info = {
+                        email : email,
+                        password: res.data
+                    }
+                    emailjs.send('kwakky1@gmail.com','template_6MwZcBCh',info,'user_hmTOnrjGGf2wvM7F3cJKY')
+                        .then((res)=> {
+                            history.push('/pages/login')
+                            alert("임시 비밀번호가 발급되었습니다.")
+                        }, () => {
+                            alert("이메일정보가 없습니다.")
+                            window.location.reload()
+                        })
 
-        if(password !== '') {
-            emailjs.send('kwakky1@gmail.com','template_6MwZcBCh',info,'user_hmTOnrjGGf2wvM7F3cJKY')
-                .then((res)=> {
-                    history.push('/pages/login')
-                    alert("임시 비밀번호가 발급되었습니다.")
-                }, (error) => {
-                    alert(error)
+                })
+                .catch(()=>{
+                    alert("이메일 정보가 없습니다.")
+                    window.location.reload()
                 })
         } else {
-            alert("이메일 정보가 없습니다.")
+            alert("이메일을 입력해주세요!")
         }
+
+
     }
 
         return (
@@ -53,6 +47,7 @@ export const ForgetPassword = () => {
                         <div className="row">
                             <div className="col-lg-6 offset-lg-3">
                                 <h2> 이메일은 100회까지만 가능합니다.</h2>
+                                <h2> 버튼 클릭 후 2초 정도의 딜레이가 있습니다.</h2>
                                 <h2>Forget Your Password</h2>
                                 <form className="theme-form" >
                                     <div className="form-row">
@@ -61,8 +56,7 @@ export const ForgetPassword = () => {
                                                    placeholder="Enter Your Email"/>
 
                                         </div>
-                                        <button type='button' className="btn btn-solid" onClick={findPassword}>이메일 확인</button>
-                                        <button type='button' onClick={sendEmail} className="btn btn-solid" >이메일 보내기</button>
+                                        <button type='button' onClick={sendEmail} className="btn btn-solid" hidden={hideButton} >이메일 보내기</button>
                                     </div>
                                 </form>
                             </div>

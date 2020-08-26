976dd3fa298bd56c8cf7b00d3449da81af1afc32
이준화 <62980975+wnsghk16@users.noreply.github.com>
2020-08-26 21:00:25 +0900

    git ignore

--- src/api/key.js
+++
@@ -1,5 +0,0 @@
-export const MAP_KEY = 'AIzaSyCch4Tqr5xXU_BYZXvR5WDPXhJoepWS-1A';
-export const SERVICE_ID = 'wnsghk666666@gmail.com'
-export const TEMPLATE_ID = 'template_qLsjeDdj'
-export const USER_ID = 'user_OyQTsPHL6IPdgN2a1LUht'
-export const IMP_ID = 'imp07844468'

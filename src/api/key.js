8f430b25ece01be3a0faf29dfe3a4d1399713241
LeeHyengTae <tedd911@icloud.com>
2020-08-26 17:02:54 +0900

    api key

---
+++ src/api/key.js
@@ -0,0 +1,5 @@
+export const MAP_KEY = 'AIzaSyBFO36_89QqRmZLMSEc_UyQA7zBUrw2TU0';
+export const SERVICE_ID = 'militaryht_gmail_com'
+export const TEMPLATE_ID = 'template_wQIYexzu'
+export const USER_ID = 'user_DTyG19MsDz0K7O2IOLMFf'
+export const IMP_ID = 'imp07844468'

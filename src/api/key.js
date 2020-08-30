064d28a06c0cb19ff8a2d823171f7538afd309c3
LeeHyengTae <tedd911@icloud.com>
2020-08-30 15:43:57 +0900

    git ignore 마스터 브런치로 병합 준비

---
+++ src/api/key.js
@@ -0,0 +1,6 @@
+export const MAP_KEY = 'googleMap Key';
+export const SERVICE_ID = 'militaryht_gmail_com'
+export const TEMPLATE_ID = 'template_wQIYexzu'
+export const USER_ID = 'user_DTyG19MsDz0K7O2IOLMFf'
+export const IMP_ID = 'imp07844468'
+export const AWS_PATH = 'http://localhost:8080'
